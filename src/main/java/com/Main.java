f70cf135e92230a8f47ce9ed4934274d6c33259a
Pedro Rodrigues <pedrohrodrigues86@gmail.com>
2025-03-27 22:47:04 -0300

    Exec 1 finalizado

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,9 +2,6 @@ package com.meuapp;
 
 import com.meuapp.controller.ScraperController;
 import com.meuapp.service.FileZipper;
-import com.meuapp.service.PdfExtractor;
-import com.meuapp.service.SaveCsv;
-import com.meuapp.service.TableExtractor;
 
 public class Main {
 
@@ -12,16 +9,6 @@ public class Main {
         ScraperController controller = new ScraperController();
         controller.iniciarScraping();
         FileZipper.compactarAnexos();
-
-        String caminhoPdf = "C:\\Users\\USER\\OneDrive\\Documentos\\estudos\\Projetos-Java\\Intuitive-care\\downloads\\Anexo I.pdf";
-        String caminhoCsv = "C:\\Users\\USER\\OneDrive\\Documentos\\estudos\\Projetos-Java\\Intuitive-care\\downloads";
-        // Chama o método para extrair texto
-        PdfExtractor.extrairTextoDePdf(caminhoPdf);
-        // Chama o método para processar a tabela
-        TableExtractor.extrairTabelaDePdf(caminhoPdf);
-        // Salvar a tabela em CSV
-        SaveCsv.salvarTabelaEmCsv(caminhoCsv);
-
         System.out.println("Programa finalizado com sucesso!");
     }
 }
